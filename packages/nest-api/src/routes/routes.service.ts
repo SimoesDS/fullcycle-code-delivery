37baa688f45354bed5d528653c8cfa05a59b925c
SimoesDS <simoes.sdgo@gmail.com>
2023-05-12 22:44:12 +0100

    adding route findAll

--- packages/nest-api/src/routes/routes.service.ts
+++ packages/nest-api/src/routes/routes.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
+import { Route, RouteDocument } from './entities/route.entity';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class RoutesService {
+  constructor(
+    @InjectModel(Route.name) private routeModel: Model<RouteDocument>,
+  ) {}
+
   create(createRouteDto: CreateRouteDto) {
     return 'This action adds a new route';
   }
 
   findAll() {
-    return `This action returns all routes`;
+    return this.routeModel.find().exec();
   }
 
   findOne(id: number) {
